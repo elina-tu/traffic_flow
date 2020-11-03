8f6b0df480fa0a880443f3bb3b806540aa5778c0
Elina <tubiselina@gmail.com>
2020-11-03 22:21:21 +0000

    acceleration and slowing down added in speed_update

--- flow_functions.py
+++ flow_functions.py
@@ -1,7 +1,17 @@
 import numpy as np
 
-def position_update(road, cars, velocity, road_lenght):
+def position_update(road, position, velocity, road_length):
     road[:] = np.NaN
-    cars = (cars + velocity)%road_lenght
-    road[np.array(cars)] = velocity
-    return road, cars
+    position = (position + velocity)%road_length
+    road[np.array(position)] = velocity
+    return road, position
+
+def speed_update(position, velocity, v_max, road_length):
+    #how far many spaces to the next car
+    space_next_car = np.diff(position, append=position[0])%road_length
+    for i in range(len(position)):
+        if (velocity[i] + 1 < space_next_car[i]) & (velocity[i] < v_max):
+            velocity[i] += 1
+        elif space_next_car[i] <= velocity[i]:
+            velocity[i] = space_next_car[i] - 1
+    return velocity
